6321a51450b8ccd0df374bc40fcc3043b7e8db80
Deepthi Ganji <115901667+deepthireddyg@users.noreply.github.com>
2022-11-04 19:26:07 -0400

    Finished Triangle Utilities

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -3,18 +3,50 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        int idx =1;
+        while (idx < numberOfRows) {
+            String thisRow = getRow(idx);
+            sb.append(thisRow).append("\n");
+            idx ++;
+        }
+
+        return sb.toString();
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0;i <numberOfStars; i++){
+            sb.append("*");
+
+        }
+        return sb.toString();
     }
 
-    public static String getSmallTriangle() {
-        return null;
+    public static String getSmallTriangle( ) {
+        int numberOfRows = 5;
+        StringBuilder sb = new StringBuilder();
+        int idx =1;
+        while (idx < numberOfRows) {
+            String thisRow = getRow(idx);
+            sb.append(thisRow).append("\n");
+            idx ++;
+        }
+
+        return sb.toString();
     }
 
-    public static String getLargeTriangle() {
-        return null;
+    public static String getLargeTriangle( ) {
+        int numberOfRows = 10;
+        StringBuilder sb = new StringBuilder();
+        int idx =1;
+        while (idx < numberOfRows) {
+            String thisRow = getRow(idx);
+            sb.append(thisRow).append("\n");
+            idx ++;
+        }
+
+        return sb.toString();
     }
 }
